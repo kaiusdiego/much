6588a3afa7ea534a55b5fbfff57eb63280559c98
kdrs <kaiusdiego@gmail.com>
2020-11-13 21:55:35 -0300

    Mapeamento de objetos, obedecendo padrão pedido para atributos do objeto de resposta.

--- src/controllers/ParserController.js
+++ src/controllers/ParserController.js
@@ -5,7 +5,14 @@ class ParserController{
     async getRecipe(search){
         var res =  axios.get(`http://www.recipepuppy.com/api/?i=${search}`)
             .then(function (response) {
-                return response.data.results;
+                return response.data.results.map((e)=>{
+                    return {
+                        title: e.title,
+                        ingredients: e.ingredients,
+                        link: e.href,
+                        gif: e.thumbnail,
+                    }
+                });
             })
             .catch(function (error) {
                 return error;
--- src/controllers/RecipesController.js
+++ src/controllers/RecipesController.js
@@ -1,7 +1,20 @@
+import ParserController from './ParserController';
 class RecipesController{
 
     async index(req, res){
-        
+        if(req.query['i'] == null || req.query['i'] == ''){
+            return res.json('Parâmetro errado.');
+        }
+
+        const rawParam = req.query['i'];
+        const keywords = rawParam.split(',').sort();
+        if(keywords.length >3){
+            return res.json('Quantidade de parâmetros ultrapassou o limite de 3 ingredientes.');            
+        }
+
+        let recipes = await ParserController.getRecipe(rawParam);
+        let nRes = {keywords,recipes}
+        return res.json(nRes);
     }
 }
 
